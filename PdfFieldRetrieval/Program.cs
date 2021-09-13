e73bb583480d06d870e780986299e4e7be001423
joshman1019 <36560776+joshman1019@users.noreply.github.com>
2021-09-13 00:25:29 -0400

    Add project files.

---
+++ PdfFieldRetrieval/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+
+namespace PdfFieldRetrieval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter form path");
+            var path = Console.ReadLine();
+            RetrieveFields(path);
+            Console.WriteLine("Done. Please press ENTER to exit this application");
+            Console.ReadLine(); 
+
+        }
+
+        static void RetrieveFields(string path)
+        {
+            try
+            {
+                PdfDocument document = new PdfDocument(new PdfReader(path)); 
+                PdfAcroForm form = PdfAcroForm.GetAcroForm(document, false);
+                var fields = form.GetFormFields();
+                foreach (string formField in fields.Keys)
+                {
+                    Console.WriteLine(formField);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); 
+                throw;
+            }
+        }
+    }
+}
